36f83ade0ccd6fb98e1cc8f3e97b1d5bc1498518
hussain <hussain.13@live.com>
2020-05-23 00:01:17 -0500

    Updates

---
+++ precache-manifest.06f17a0e30386d5d7f5faf9c433be2fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8745fc97de52013cf63363b7df4ef40",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "30509ec908384f884a43",
+    "url": "/Monsters/static/css/main.c8dff053.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/Monsters/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30509ec908384f884a43",
+    "url": "/Monsters/static/js/main.37569b20.chunk.js"
+  },
+  {
+    "revision": "bd32f950720354cfb88c",
+    "url": "/Monsters/static/js/runtime-main.903ad071.js"
+  }
+]);
